09fc004be1664733a83ca561dec2f5fb981b8524
alexeynau <firnen76@gmail.com>
2023-06-03 15:01:39 +0300

    Add common colors

---
+++ custom_chat_gpt/lib/common/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color mainBackground = Color.fromARGB(255, 12, 12, 12);
+  static const Color cellBackground = Color.fromARGB(255, 29, 29, 29);
+  static const Color inputFieldBackground = Color(0xFF353535);
+  static const Color iconBlueColor = Color(0xFF0C89E9); 
+  static const Color iconRedColor = Color.fromARGB(255, 221, 39, 39);
+  static const Color dividerColor = Color(0xFF777777);
+  static const Color regenerateButtonBorderColor = Color(0xFFA5A5A5);
+}
